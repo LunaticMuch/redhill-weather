1362e410579deb12dbe8f89a6094e857a6cce23a
LunaticMuch <s.cislaghi@gmail.com>
2022-01-28 20:13:26 +0000

    Initial Commit

---
+++ Redhill Weather/Redhill_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redhill_WeatherApp.swift
+//  Redhill Weather
+//
+//  Created by Stefano Cislaghi on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Redhill_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
